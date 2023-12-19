c132ac40bb86e6b4c2a970b3a7d08d3c002cd12d
davidbujok <davidbujok01@gmail.com>
2023-12-19 23:48:13 +0000

    Implementing database with ChatGpt text generation

--- generate_personal_statement.py
+++ generate_personal_statement.py
@@ -1,47 +1,92 @@
-from os import close
 from openai import OpenAI
-import pprint
+import psycopg2
+from psycopg2.errors import Error
 
 client = OpenAI()
 
-jobs = open("job_offers.txt", "r")
-jobs_sorted = []
-job = ""
-for line in jobs:
-    if line.startswith("---"):
-        jobs_sorted.append(job)
-        job = ""
-        pass
-    else:
-        job += f"{line.strip()} \n"
+connection = psycopg2.connect(
+    database="mydatabase", host="localhost", user="arch", password="", port=5432
+)
+selected_id = 64
+find_job_position_by_id_query = 'SELECT * FROM jobs WHERE id = %s'
+job_id = None
+try:
+    with connection.cursor() as cursor:
+        try:
+            # job_data = cursor.execute(find_job_level_by_id_query, ( selected_id, ))
+            # job_data = cursor.execute(find_job_position_by_id_query, ( selected_id, ))
+            job_data = cursor.execute(find_job_position_by_id_query, ( selected_id, ))
+            job_id = cursor.fetchall()
+            print(job_id[0][1])
+            print(job_id[0][2])
+            print(job_id[0][3])
+            print(job_id[0][4])
+            # for info in job_id:
+            #     print(info)
+        except Exception as e:
+            connection.rollback()
+            print(f"Error inserting into websites table: {e}")
+except Error as e:
+    print("Database connection error: ", e)
 
-print(len(jobs_sorted))
-print(jobs_sorted[0])
-print("-----------------------------------------------------------------")
-jobs.close()
-cv = open("cv.txt", "r")
-cv_text = ""
-for line in cv:
-    cv_text += line
+try:
+    if job_id != None:
+        job_level = job_id[0][1]
+        job_position = job_id[0][2]
+        company_info  = job_id[0][3]
+        job_details  = job_id[0][4]
+except UnboundLocalError:
+    print(f"Job id doesn't exisit")
 
-prompt_user = """Considering education and job history of the individaul below and job description 
-write Personal Statement for the CV. Focus on the most recent acomplishments of the individaul
-especially CodeClan Bootcamp as he's seeking the role as a Junior Software developer. 
+with open("cv.txt", "r") as cv:
+    cv_text = cv.read()
+
+prompt_personal_statement = f"""Considering education and job history of this individaul {cv_text} who wants to 
+apply for this position: {job_position} of this level: {job_level} for this comapny: {company_info}
+write Personal Statement for the CV. When writing the personal statement consider job details in 
+here: {job_details} and how they align with candidate experiance, programming languages and
+technologies. Focus on the most recent acomplishments of the individaul especially CodeClan
+Bootcamp as he's seeking the role as a Junior or Entry Level Software developer. 
 Try to tie his experiance and soft skill and how they can be helpful for the company in the job 
 description. Made Personal statement six to eight sentences focusing mainly on technologies that
 individaul knows and how they are tightly connected with technologies in the job description"""
 
 
-completion = client.chat.completions.create(
-    model="gpt-3.5-turbo",
+personal_statement = client.chat.completions.create(
+    model="gpt-4-1106-preview",
     messages=[
         {
-            "role": "user",
-            "content": f"{prompt_user} CV: {cv_text} and here's job description {jobs_sorted[0]}",
+            "role": "assistant",
+            "content": f"{prompt_personal_statement}",
         }
     ],
 )
 
-# print(completion)
-# print(type(completion))
-__import__("pprint").pprint(f"{ completion.choices[0].message.content }")
+prompt_user_email = f"""Considering position: {job_position}, level of the job: {job_level}, 
+company: {company_info} and job details: {job_details} as well as 
+{personal_statement.choices[0].message.content} write short e-mail that will be send along with cv
+to apply for a job"""
+
+e_mail = client.chat.completions.create(
+    model="gpt-4-1106-preview",
+    messages=[
+        {
+            "role": "assistant",
+            "content": f"{prompt_user_email}",
+        }
+    ],
+)
+
+with open("personal_statement.txt", "w") as ps:
+    if personal_statement.choices[0].message.content is not None:
+        ps.write(f"PERSONAL STATEMENT\n")
+        ps.write(personal_statement.choices[0].message.content)
+    if e_mail.choices[0].message.content is not None:
+        ps.write(f"E-MAIL\n")
+        ps.write(e_mail.choices[0].message.content)
+
+
+# # print(completion)
+# # print(type(completion))
+# __import__("pprint").pprint(f"{ personal_statement.choices[0].message.content }")
+print("Completed")
