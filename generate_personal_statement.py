50e4c962f8e098316880c27629c634ad89da4f7b
davidbujok <davidbujok01@gmail.com>
2023-12-29 13:28:55 +0000

    Add new data and modify the prompts

--- generate_personal_statement.py
+++ generate_personal_statement.py
@@ -7,22 +7,29 @@ client = OpenAI()
 connection = psycopg2.connect(
     database="mydatabase", host="localhost", user="arch", password="", port=5432
 )
-selected_id = 64
+selected_id = input() 
 find_job_position_by_id_query = 'SELECT * FROM jobs WHERE id = %s'
 job_id = None
+job_level = 'Junior Software Developer'
+job_position = None
+company_name = None
+company_values = None
+job_details = None
+job_url = None
 try:
     with connection.cursor() as cursor:
         try:
-            # job_data = cursor.execute(find_job_level_by_id_query, ( selected_id, ))
-            # job_data = cursor.execute(find_job_position_by_id_query, ( selected_id, ))
             job_data = cursor.execute(find_job_position_by_id_query, ( selected_id, ))
             job_id = cursor.fetchall()
-            print(job_id[0][1])
-            print(job_id[0][2])
-            print(job_id[0][3])
-            print(job_id[0][4])
-            # for info in job_id:
-            #     print(info)
+            # print('job_level',job_id[0][1])
+            # print('job_position',job_id[0][2])
+            # print('about_long_section',job_id[0][3])
+            # print('job_url',job_id[0][4])
+            # print('website_id',job_id[0][5])
+            # print('job_id',job_id[0][6])
+            # print('job_post_date',job_id[0][7])
+            # print('comapny_name',job_id[0][8])
+            # print('job_location',job_id[0][9])
         except Exception as e:
             connection.rollback()
             print(f"Error inserting into websites table: {e}")
@@ -33,20 +40,21 @@ try:
     if job_id != None:
         job_level = job_id[0][1]
         job_position = job_id[0][2]
-        company_info  = job_id[0][3]
-        job_details  = job_id[0][4]
+        company_name = job_id[0][8]
+        company_values = job_id[0][3]
+        job_url =  job_id[0][4]
 except UnboundLocalError:
     print(f"Job id doesn't exisit")
 
 with open("cv.txt", "r") as cv:
     cv_text = cv.read()
 
-prompt_personal_statement = f"""Considering education and job history of this individaul {cv_text} who wants to 
-apply for this position: {job_position} of this level: {job_level} for this comapny: {company_info}
-write Personal Statement for the CV. When writing the personal statement consider job details in 
-here: {job_details} and how they align with candidate experiance, programming languages and
-technologies. Focus on the most recent acomplishments of the individaul especially CodeClan
-Bootcamp as he's seeking the role as a Junior or Entry Level Software developer. 
+prompt_personal_statement = f"""Considering education and job history of this individaul {cv_text}
+who wants to apply for this position: {job_position} of this level: {job_level} for this comapny:
+{company_name} write Personal Statement for the CV. When writing the personal statement consider
+job details in here: {company_values} and how they align with candidate experiance such as programming
+languages and technologies. Focus on the most recent acomplishments of the individaul especially
+CodeClan Bootcamp as he's seeking the role as a Junior or Entry Level Software developer. 
 Try to tie his experiance and soft skill and how they can be helpful for the company in the job 
 description. Made Personal statement six to eight sentences focusing mainly on technologies that
 individaul knows and how they are tightly connected with technologies in the job description"""
@@ -63,7 +71,7 @@ personal_statement = client.chat.completions.create(
 )
 
 prompt_user_email = f"""Considering position: {job_position}, level of the job: {job_level}, 
-company: {company_info} and job details: {job_details} as well as 
+company: {company_name} and job details: {company_values} as well as 
 {personal_statement.choices[0].message.content} write short e-mail that will be send along with cv
 to apply for a job"""
 
@@ -85,8 +93,4 @@ with open("personal_statement.txt", "w") as ps:
         ps.write(f"E-MAIL\n")
         ps.write(e_mail.choices[0].message.content)
 
-
-# # print(completion)
-# # print(type(completion))
-# __import__("pprint").pprint(f"{ personal_statement.choices[0].message.content }")
-print("Completed")
+print(job_url)
